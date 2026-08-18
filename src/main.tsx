3107c02a8682b79ac8eb5693465bef8d3bef9ac0
myothuonion-ui <myothuonion@gmail.com>
2026-08-18 22:17:09 +0630

    Show API key boxes directly in local app (#8)
    * Add always-visible quick API key panel
    
    * Style quick API key panel
    
    * Render quick API key panel in local app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { QuickApiSettings } from './QuickApiSettings';
 import './styles.css';
 import './factory-v2.css';
 import './factory-v3.css';
+import './quick-api.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <QuickApiSettings />
     <App />
   </React.StrictMode>
 );
