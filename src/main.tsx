3f992bb994477f4ac4b18e7e8ae5d9f1c747f806
myothuonion-ui <myothuonion@gmail.com>
2026-08-19 11:15:49 +0630

    Render one-click hybrid 40Q workflow

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { QuickApiSettings } from './QuickApiSettings';
+import { OneClickBuild } from './OneClickBuild';
 import './styles.css';
 import './factory-v2.css';
 import './factory-v3.css';
 import './quick-api.css';
+import './one-click.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QuickApiSettings />
+    <OneClickBuild />
     <App />
   </React.StrictMode>
 );
