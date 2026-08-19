01154278c132757edf864c05368f7eb3e320172f
myothuonion-ui <myothuonion@gmail.com>
2026-08-19 11:47:26 +0630

    Switch local UI to simplified v0.5 workspace

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { AppV5 } from './AppV5';
 import { QuickApiSettings } from './QuickApiSettings';
 import { OneClickBuild } from './OneClickBuild';
 import './styles.css';
@@ -8,11 +8,12 @@ import './factory-v2.css';
 import './factory-v3.css';
 import './quick-api.css';
 import './one-click.css';
+import './v5.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QuickApiSettings />
     <OneClickBuild />
-    <App />
+    <AppV5 />
   </React.StrictMode>
 );
